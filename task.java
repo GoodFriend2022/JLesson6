b5dc928406b2c3583b46fa609443590045028b65
Uri_Klevcov <a-08-137.mail.ru>
2023-03-04 16:22:30 +0600

    finish

--- task.java
+++ task.java
@@ -2,32 +2,22 @@ import java.util.ArrayList;
 
 public class task {
     public static void main(String[] args) {
-        task();
+        ArrayList<Integer> list = UserTree();
+        System.out.println(list);
     }
 
-    static void task() {
-        treenode tree = new treenode(1,null, new treenode(2,null,new treenode(3)));
-        System.out.println(preorderTraversal(tree));
+    static ArrayList<Integer> UserTree() {
+        treenode tree = new treenode(1, null, new treenode(2, null, new treenode(3)));
+        return PreorderTraversal(tree);
     }
 
-    static ArrayList<Integer> preorderTraversal(treenode root) {
+    static ArrayList<Integer> PreorderTraversal(treenode root) {
         ArrayList<Integer> array = new ArrayList<>();
         if (root != null) {
             array.add(root.val);
-            array.addAll(preorderTraversal(root.left));
-            array.addAll(preorderTraversal(root.right));
+            array.addAll(PreorderTraversal(root.left));
+            array.addAll(PreorderTraversal(root.right));
         }
         return array;
     }
-
-    public void fillTree(treenode root, int k) {
-        if (root == null) {
-            root.val = k;
-        } else if (root.left == null) {
-            fillTree(root.left, k);
-        } else {
-            fillTree(root.right, k);
-        }
-    }
-
 }
